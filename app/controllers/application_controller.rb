679dffda5def8e80908023f9d7acda095923f856
Pof Magicfingers <pof@podshows.fr>
2019-10-01 01:11:22 +0200

    fix: allow home without password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :check_password
+  before_filter :check_password, :except => [:home]
 
   def home
   end
